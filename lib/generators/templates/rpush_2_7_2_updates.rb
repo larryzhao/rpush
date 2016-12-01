841cbf6666f9d20f71eb6557c222e188b4b69bef
Larry Zhao <thehiddendepth@gmail.com>
2016-12-02 01:29:53 +0800

    change name to fetch_prio

--- lib/generators/templates/rpush_2_7_2_updates.rb
+++ lib/generators/templates/rpush_2_7_2_updates.rb
@@ -1,9 +1,9 @@
-class Rpush271Updates < ActiveRecord::Migration
+class Rpush272Updates < ActiveRecord::Migration
   def self.up
-    add_column :rpush_notifications, :prio, :integer, limit: 1, default: 1
+    add_column :rpush_notifications, :fetch_prio, :integer, limit: 1, default: 1
   end
 
   def self.down
-    remove_column :rpush_notifications, :prio
+    remove_column :rpush_notifications, :fetch_prio
   end
 end
